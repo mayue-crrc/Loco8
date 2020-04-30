ee1676665eae6382b299298149fe6f7acce9a530
guochangfu <g280651957@gmail.com>
2020-04-30 10:04:10 +0800

    Device_VersionV1.1

--- devicedata_version.h
+++ devicedata_version.h
@@ -2,6 +2,7 @@
 #define DEVICEDATA_VERSION_H
 
 #include "mybase.h"
+#include "QLabel"
 
 namespace Ui {
 class DeviceData_Version;
@@ -14,9 +15,12 @@ class DeviceData_Version : public MyBase
 public:
     explicit DeviceData_Version(QWidget *parent = 0);
     ~DeviceData_Version();
+    void updatePage();
 
 private:
     Ui::DeviceData_Version *ui;
+    QList<QLabel*> Device,VerX,VerY,VerZ;
+    QList<QString> NameList;
 };
 
 #endif // DEVICEDATA_VERSION_H
